0dd933fbe3f9f19bfc7dc88e9554f755d4d1ad5c
aes-sarg <ayoubmajd311@gmail.com>
2023-11-03 22:34:13 +0100

    add ft_memchr function

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,20 +6,22 @@
 /*   By: aes-sarg <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 17:21:59 by aes-sarg          #+#    #+#             */
-/*   Updated: 2023/11/02 17:53:00 by aes-sarg         ###   ########.fr       */
+/*   Updated: 2023/11/03 21:40:55 by aes-sarg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t	i;
-	char	*ptr;
+	unsigned int	i;
+	unsigned char	*ptr;
+	unsigned char	c2;
 
+	c2 = (unsigned char)c;
 	i = 0;
-	ptr = (char *)s;
+	ptr = (unsigned char *)s;
 	while (ptr[i] != '\0' && i < n)
 	{
-		if (ptr[i] == c)
+		if (ptr[i] == c2)
 		{
 			return (ptr + i);
 		}
