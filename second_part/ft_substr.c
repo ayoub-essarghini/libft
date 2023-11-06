5feacfd2c185ac50fff191d60fe9ce7ece9d8e06
aes-sarg <aes-sarg@e1r2p12.1337.ma>
2023-11-06 19:20:00 +0100

    add new function ft_substr

---
+++ second_part/ft_substr.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aes-sarg <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/06 18:00:00 by aes-sarg          #+#    #+#             */
+/*   Updated: 2023/11/06 18:42:10 by aes-sarg         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+char *ft_strncpy(char *dst,const char *src,size_t len)
+{
+	size_t i;
+	char	*s;
+	char	d;
+
+	s = (char *) src;
+	d = (char *) dst;
+	i = 0;
+	while (src[i] != '\0' && i < len)
+		d[i] = s[i];
+		i++;
+	return (dst);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{	
+	size_t	origin_len;
+	size_t	substr_len;
+	char	*sub;
+
+	origin_len = ft_strlen(s);
+	if (start >= origin_len)
+		return (NULL);
+	if (start + len >= origin_len)
+		substr_len = origin_len - size;
+	else
+		substr_len = len;
+	sub = (char *)malloc((substr_len + 1) * sizeof(char));
+	if (sub == NULL)
+		return (NULL);
+	ft_strncpy(sub,s+start,substr_len);
+	sub[substr_len] = '\0';
+	return (sub);
+}
